2397a321d55daea29ad84261054271faa738cd2e
Kenichi Kamiya <kachick1@gmail.com>
2022-10-31 21:43:22 +0900

    Using tempfile might be better

--- commit_if_ok.bash
+++ commit_if_ok.bash
@@ -6,6 +6,8 @@ set -euo pipefail
 # Then trying to build exercise No.42, and commit if passed
 
 exercise="$1"
+tmpfile="$(mktemp)"
 
 # `zig build number` exit 0 even if faling the build!
-(zig build "$exercise" 2>&1 | grep -P '^PASSED:') && git commit -a -m "OK \'zig build $exercise\`"
+zig build "$exercise" 2>&1 | tee --append "$tmpfile"
+(grep -P '^PASSED:' < "$tmpfile") && git commit -a -m "OK \'zig build $exercise\`"
